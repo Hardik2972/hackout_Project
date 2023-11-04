835190bb420223fd314487c3116c4c465dfd5d0c
Ayush Gupta <ag8350961@gmail.com>
2023-11-05 00:47:01 +0530

    added alert & admin fixes

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,7 +1,8 @@
-const {handleReport} = require("../controller/adminFunc");
+const {handleReport, handleCount} = require("../controller/adminFunc");
 const express= require("express");
 const router= express.Router();
 
 router.get("/report",handleReport);
+router.get("/count",handleCount);
 
 module.exports=router;
--- server/routes/newUser.js
+++ server/routes/newUser.js
@@ -1,6 +1,6 @@
 const express= require("express");
 const router= express.Router();
-const {handleLogin , handleSignup , handleAuthentication}= require("../controller/user");
+const {handleLogin , handleSignup , handleAuthentication, handleSms}= require("../controller/user");
 
 router.get("/",(req,res) => {
     res.redirect("/login");
@@ -19,5 +19,8 @@ res.status(300).json({url: "/signup"});
 .post(handleSignup);
 
 router.post("/authenticate",handleAuthentication);
+
+router.route("/sms")
+.get(handleSms)
 
 module.exports=router;
